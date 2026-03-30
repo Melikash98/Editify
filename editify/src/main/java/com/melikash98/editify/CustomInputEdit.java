424476f5492bb2012eea978a230bcde1ba18724d
Melika Shooryabi <138528779+Melikash98@users.noreply.github.com>
2026-03-30 10:08:36 +0330

    update

--- editify/src/main/java/com/melikash98/editify/CustomInputEdit.java
+++ editify/src/main/java/com/melikash98/editify/CustomInputEdit.java
@@ -99,7 +99,7 @@ public class CustomInputEdit extends ConstraintLayout {
             float distanceToTop = (editInput.getHeight() / 2f) - (hintLayout.getHeight() / 2f);
 
             // یه مقدار خیلی کم برای اینکه دقیق روی خط بشینه
-            float adjust = dp(12);
+            float adjust = dp(14);
 
             float targetY = shouldFloat
                     ? -(distanceToTop + adjust)   // بره روی خط
